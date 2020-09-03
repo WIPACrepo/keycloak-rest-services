dd44f301258b3366c3a31b26749ff4c0b91cd52c
Ric Evans <emejqz@gmail.com>
2020-09-03 15:26:17 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 
 import glob
 import os
+import re
 import sys
 
+PACKAGE = "keycloak-rest-services"
+
 if sys.version_info < (3, 6):
-    print("ERROR: rest_tools requires at least Python 3.6+ to run.")
+    print(f"ERROR: {PACKAGE} requires at least Python 3.6+ to run.")
     sys.exit(1)
 
 try:
@@ -21,10 +24,11 @@ kwargs = {}
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 
-with open(os.path.join(current_path, "rest_tools", "__init__.py")) as f:
+with open(os.path.join(current_path, "__init__.py")) as f:
     for line in f.readlines():
         if "__version__" in line:
-            kwargs["version"] = line.split("=")[-1].split("'")[1]
+            PATTERN = r"""__version__ = ('|")(?P<v>.+)('|")"""
+            kwargs["version"] = re.match(PATTERN, line).groupdict()["v"]
             break
     else:
         raise Exception("cannot find __version__")
@@ -35,41 +39,55 @@ with open(os.path.join(current_path, "README.md")) as f:
 
 if setuptools is not None:
     # If setuptools is not available, you're on your own for dependencies.
-    install_requires = [
-        "tornado>=5.1",
-        "requests",
-        "requests_toolbelt",
-        "requests-futures",
-        "sphinx>=1.4",
-        "coverage>=4.4.2",
-        "requests-mock",
-        "PyJWT",
-        "cryptography",
-    ]
+    install_requires = []
+    dependency_links = []
+    with open(os.path.join(current_path, "requirements.txt")) as f:
+        for line in f.readlines():
+            # GitHub Dependency
+            if m := re.match(r"(-e git:|-e git\+|git:|git\+)(?P<src>.*)", line):
+                src = m.groupdict()["src"]
+                if m := re.match(r"(?P<url>.*)(?P<egg>#egg=)(?P<pkg>.*)", src):
+                    # add tarball master link
+                    tarball_link = f"{m['url']}/tarball/master{m['egg']}{m['pkg']}"
+                    dependency_links.append(tarball_link)
+                    # add package name
+                    install_requires.append(m["pkg"])
+                else:
+                    print("Dependency to a git repository should have the format:")
+                    print("git+[ssh|https]://git@github.com/xxxx/xxxx#egg=package_name")
+            # Don't Add PyTests
+            elif "pytest" in line:
+                continue
+            # PyPI Dependency
+            else:
+                install_requires.append(line)
+
     kwargs["install_requires"] = install_requires
+    kwargs["dependency_links"] = dependency_links
     kwargs["zip_safe"] = False
 
+
 setup(
-    name="rest_tools",
+    name=PACKAGE,
     scripts=glob.glob("bin/*"),
-    packages=["rest_tools", "rest_tools.client", "rest_tools.server"],
+    packages=["krs", "resources"],
     package_data={
         # data files need to be listed both here (which determines what gets
         # installed) and in MANIFEST.in (which determines what gets included
         # in the sdist tarball)
-        #'rest_utils.server':['data/etc/*','data/www/*','data/www_templates/*'],
+        # 'rest_utils.server':['data/etc/*','data/www/*','data/www_templates/*'],
     },
     author="IceCube Collaboration",
     author_email="developers@icecube.wisc.edu",
-    url="https://github.com/WIPACrepo/rest-tools",
-    license="https://github.com/WIPACrepo/rest-tools/blob/master/LICENSE",
-    description="REST tools in python - common code for client and server ",
+    url=f"https://github.com/WIPACrepo/{PACKAGE}",
+    license=f"https://github.com/WIPACrepo/{PACKAGE}/blob/master/LICENSE",
+    description="Services surrounding KeyCloak, that use the REST API to read/update state",
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Operating System :: POSIX :: Linux",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: System :: Distributed Computing",
@@ -80,5 +98,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    **kwargs
+    **kwargs,
 )
