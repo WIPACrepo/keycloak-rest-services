c24d9724494345841d47cd0b4fb561db5dfbff92
David Schultz <davids24@gmail.com>
2021-03-02 17:14:08 -0600

    add more tests for modifying a user in ldap. change parmaters for create_user to individual args instead of a dict

--- tests/krs/test_ldap.py
+++ tests/krs/test_ldap.py
@@ -39,12 +39,7 @@ def test_get_users_none(ldap_bootstrap):
         ldap_bootstrap.get_user('foo')
 
 def test_create_user(ldap_bootstrap):
-    ldap_bootstrap.create_user({
-        'username': 'foo',
-        'firstName': 'foo',
-        'lastName': 'bar',
-        'email': 'foo@bar',
-    })
+    ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar', email='foo@bar')
 
     ret = ldap_bootstrap.get_user('foo')
     assert ret['uid'] == 'foo'
@@ -54,37 +49,50 @@ def test_create_user(ldap_bootstrap):
 
 def test_create_user_fail(ldap_bootstrap):
     with pytest.raises(Exception):
-        ldap_bootstrap.create_user({
-            'username': 'foo',
-            'firstName': 'foo',
-            'email': 'foo@bar',
-        })
+        ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar')
 
     with pytest.raises(KeyError):
         ldap_bootstrap.get_user('foo')
 
-def test_modify_user(ldap_bootstrap):
-    ldap_bootstrap.create_user({
-        'username': 'foo',
-        'firstName': 'foo',
-        'lastName': 'bar',
-        'email': 'foo@bar',
-    })
-
-    ldap_bootstrap.modify_user('foo', 'posixAccount', {'gidNumber': 1234, 'uidNumber': 1234, 'homeDirectory': f'/home/foo'})
+def test_modify_user_class(ldap_bootstrap):
+    ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar', email='foo@bar')
+    ldap_bootstrap.modify_user('foo', {'gidNumber': 1234, 'uidNumber': 1234, 'homeDirectory': f'/home/foo'}, objectClass='posixAccount')
     ret = ldap_bootstrap.get_user('foo')
     assert ret['gidNumber'] == 1234
+    assert 'posixAccount' in ret['objectClass']
+
+def test_modify_user_remove_class(ldap_bootstrap):
+    ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar', email='foo@bar')
+    ldap_bootstrap.modify_user('foo', {'gidNumber': 1234, 'uidNumber': 1234, 'homeDirectory': f'/home/foo'}, objectClass='posixAccount')
+    ldap_bootstrap.modify_user('foo', {'gidNumber': None, 'uidNumber': None, 'homeDirectory': None}, removeObjectClass='posixAccount')
+    ret = ldap_bootstrap.get_user('foo')
+    assert 'gidNumber' not in ret
+    assert 'posixAccount' not in ret['objectClass']
+
+def test_modify_user_attrs(ldap_bootstrap):
+    ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar', email='foo@bar')
+    ldap_bootstrap.modify_user('foo', {'givenName': 'foofoo'})
+    ret = ldap_bootstrap.get_user('foo')
+    assert ret['givenName'] == 'foofoo'
 
 def test_modify_user_fail(ldap_bootstrap):
-    ldap_bootstrap.create_user({
-        'username': 'foo',
-        'firstName': 'foo',
-        'lastName': 'bar',
-        'email': 'foo@bar',
-    })
+    ldap_bootstrap.create_user(username='foo', firstName='foo', lastName='bar', email='foo@bar')
 
     with pytest.raises(Exception):
-        ldap_bootstrap.modify_user('foo', 'posixAccount', {'gidNumber': 1234, 'uidNumber': 1234})
+        # missing homeDirectory for posixAccount
+        ldap_bootstrap.modify_user('foo', {'gidNumber': 1234, 'uidNumber': 1234}, objectClass='posixAccount')
 
     ret = ldap_bootstrap.get_user('foo')
     assert 'gidNumber' not in ret
+
+    with pytest.raises(Exception):
+        # cannot add field gidNumber with current objectClasses
+        ldap_bootstrap.modify_user('foo', {'gidNumber': 1234})
+
+    ret = ldap_bootstrap.get_user('foo')
+    assert 'gidNumber' not in ret
+
+    ldap_bootstrap.modify_user('foo', {'gidNumber': 1234, 'uidNumber': 1234, 'homeDirectory': f'/home/foo'}, objectClass='posixAccount')
+    with pytest.raises(Exception):
+        # cannot remove objectClass without removing attrs
+        ldap_bootstrap.modify_user('foo', removeObjectClass='posixAccount')
