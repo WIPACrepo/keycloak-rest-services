c68e8ef21564991e6792a8f35b41d4afd704491e
David Schultz <davids24@gmail.com>
2021-03-25 15:33:12 -0500

    add action to create home directories

---
+++ tests/actions/test_create_home_directory.py
@@ -0,0 +1,80 @@
+import pytest
+
+#from krs.token import get_token
+from krs import users, groups, bootstrap
+from actions import create_home_directory
+
+from ..util import keycloak_bootstrap, ldap_bootstrap
+
+
+@pytest.mark.asyncio
+async def test_create(keycloak_bootstrap, tmp_path):
+    attrs = {
+        'homeDirectory': '/home/testuser',
+        'uidNumber': 12345,
+        'gidNumber': 12345,
+    }
+    await users.create_user('testuser', first_name='first', last_name='last', email='foo@test', attribs=attrs, rest_client=keycloak_bootstrap)
+
+    await create_home_directory.process(tmp_path, keycloak_client=keycloak_bootstrap)
+
+    ret_path = tmp_path / 'home/testuser'
+    assert ret_path.is_dir()
+
+@pytest.mark.asyncio
+async def test_missing_homeDirectory(keycloak_bootstrap, tmp_path):
+    attrs = {
+        'uidNumber': 12345,
+        'gidNumber': 12345,
+    }
+    await users.create_user('testuser', first_name='first', last_name='last', email='foo@test', attribs=attrs, rest_client=keycloak_bootstrap)
+
+    await create_home_directory.process(tmp_path, keycloak_client=keycloak_bootstrap)
+
+    ret_path = tmp_path / 'home/testuser'
+    assert not ret_path.is_dir()
+
+@pytest.mark.asyncio
+async def test_missing_uidNumber(keycloak_bootstrap, tmp_path):
+    attrs = {
+        'homeDirectory': '/home/testuser',
+        'gidNumber': 12345,
+    }
+    await users.create_user('testuser', first_name='first', last_name='last', email='foo@test', attribs=attrs, rest_client=keycloak_bootstrap)
+
+    await create_home_directory.process(tmp_path, keycloak_client=keycloak_bootstrap)
+
+    ret_path = tmp_path / 'home/testuser'
+    assert not ret_path.is_dir()
+
+@pytest.mark.asyncio
+async def test_missing_gidNumber(keycloak_bootstrap, tmp_path):
+    attrs = {
+        'homeDirectory': '/home/testuser',
+        'uidNumber': 12345,
+    }
+    await users.create_user('testuser', first_name='first', last_name='last', email='foo@test', attribs=attrs, rest_client=keycloak_bootstrap)
+
+    await create_home_directory.process(tmp_path, keycloak_client=keycloak_bootstrap)
+
+    ret_path = tmp_path / 'home/testuser'
+    assert not ret_path.is_dir()
+
+@pytest.mark.asyncio
+async def test_already_exists(keycloak_bootstrap, tmp_path):
+    # if the path already exists, don't do anything, even if it's incorrect
+    attrs = {
+        'homeDirectory': '/home/testuser',
+        'uidNumber': 12345,
+        'gidNumber': 12345,
+    }
+    await users.create_user('testuser', first_name='first', last_name='last', email='foo@test', attribs=attrs, rest_client=keycloak_bootstrap)
+
+    ret_path = tmp_path / 'home/testuser'
+    (tmp_path / 'home').mkdir()
+    ret_path.touch()
+
+    await create_home_directory.process(tmp_path, keycloak_client=keycloak_bootstrap)
+
+    assert ret_path.exists()
+    assert not ret_path.is_dir()
