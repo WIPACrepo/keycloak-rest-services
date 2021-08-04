7f5232e62ae88a3393c70603f2718248128889e3
David Schultz <davids24@gmail.com>
2021-08-04 15:31:46 -0500

    add email sending and tests

---
+++ tests/krs/test_email.py
@@ -0,0 +1,27 @@
+import smtplib
+from contextlib import AbstractContextManager
+from unittest.mock import MagicMock
+
+import pytest
+
+import krs.email
+
+
+class MockResponse(AbstractContextManager, MagicMock):
+    def __exit__(self, *args):
+        pass
+    send_message = MagicMock()
+
+@pytest.fixture
+def patch_smtp(mocker):
+    return mocker.patch('smtplib.SMTP', new=MockResponse())
+
+def test_send_email(patch_smtp):
+    krs.email.send_email('foo@bar', 'test', 'test message')
+
+    patch_smtp.assert_called()
+    patch_smtp.return_value.send_message.assert_called()
+    a = patch_smtp.return_value.send_message.call_args[0][0]
+    assert 'To: foo@bar' in str(a)
+    assert 'Subject: test' in str(a)
+    assert 'test message' in str(a)
