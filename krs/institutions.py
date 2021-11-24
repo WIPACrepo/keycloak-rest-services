3f1475e3727125af67b645e97cf48df197282e27
David Schultz <davids24@gmail.com>
2021-11-24 15:07:54 -0600

    add institution helpers

---
+++ krs/institutions.py
@@ -0,0 +1,219 @@
+"""
+Institution actions against Keycloak.
+"""
+import asyncio
+from enum import Enum
+import json
+import logging
+
+from .users import user_info, _fix_attributes
+from . import groups
+from .token import get_rest_client
+
+logger = logging.getLogger('krs.institutions')
+
+
+class Region(Enum):
+    """Institution region"""
+    NORTH_AMERICA = "North America"
+    EUROPE = "Europe"
+    ASIA_PAC = "Asia Pacific"
+
+
+async def list_insts(experiment=None, filter_func=None, rest_client=None):
+    """
+    List institution group_paths in Keycloak.
+
+    Args:
+        experiment (str): experiment name (default: list all experiments)
+        filter_func (callable): given a group path and set of attrs, a callable to return true/false
+
+    Returns:
+        dict: group_path: attrs
+    """
+    url = '/groups?briefRepresentation=false&search=institutions'
+    group_hierarchy = await rest_client.request('GET', url)
+    ret = {}
+
+    def add_groups(groups):
+        for g in groups:
+            if g['subGroups']:
+                add_groups(g['subGroups'])
+            parts = g['path'].split('/')
+            if len(parts) == 4 and parts[1] == 'institutions':
+                # this is an institution group
+                if experiment and parts[2] != experiment:
+                    continue
+                _fix_attributes(g)
+                authorlists = {}
+                for subg in g['subGroups']:
+                    if subg['name'].startswith('authorlist-'):
+                        _fix_attributes(subg)
+                        authorlists[subg['name'].replace('authorlist-','')] = subg['attributes'].get('cite','')
+                if authorlists:
+                    g['attributes']['authorlists'] = authorlists
+                if callable(filter_func) and not filter_func(g['path'], g['attributes']):
+                    continue
+                ret[g['path']] = g['attributes']
+    add_groups(group_hierarchy)
+
+    return {k:ret[k] for k in sorted(ret)}
+
+class InstitutionAttrsMismatchError(RuntimeError):
+    pass
+
+async def list_insts_flat(experiment=None, filter_func=None, rest_client=None):
+    """
+    List institution names in Keycloak, removing overlaps from multiple experiments.
+
+    Args:
+        experiment (str): experiment name (default: list all experiments)
+        filter_func (callable): given a group path and set of attrs, a callable to return true/false
+
+    Returns:
+        dict: name: attrs
+    """
+    raw = await list_insts(experiment, filter_func=filter_func, rest_client=rest_client)
+    ret = {}
+    for path in raw:
+        inst_key = path.split('/')[-1]
+        attrs = raw[path]
+        if inst_key in ret and ret[inst_key] != attrs:
+            logger.warning(f'inst attr mismatch - {inst_key}')
+            raise InstitutionAttrsMismatchError(inst_key)
+        ret[inst_key] = attrs
+    return ret
+
+async def inst_info(experiment, institution, rest_client=None):
+    """
+    Get institution information.
+
+    Args:
+        experiment (str): experiment name
+        institution (str): institution name
+
+    Returns:
+        dict: attribute info
+    """
+    group_path = f'/institutions/{experiment}/{institution}'
+    ret = await list_insts(experiment, rest_client=rest_client)
+    if group_path not in ret:
+        raise KeyError(f'inst "{group_path}" does not exist')
+    return ret[group_path]
+
+def validate_attrs(attrs):
+    if not attrs:
+        raise ValueError('attrs is required')
+    for n in ('name', 'cite', 'abbreviation', 'is_US', 'region'):
+        if n not in attrs:
+            raise KeyError(f'{n} is required in attrs')
+    if isinstance(attrs['region'], Region):
+        attrs['region'] = attrs['region'].value
+    else:
+        Region(attrs['region']) # this will raise a ValueError if not a Region
+    if 'authorlist' not in attrs:
+        attrs['authorlist'] = False
+    if 'has_mou' not in attrs:
+        attrs['has_mou'] = False
+    return attrs
+
+async def create_inst(experiment, institution, attrs=None, rest_client=None):
+    """
+    Create an institution in Keycloak.
+
+    Requred attributes:
+        name (str): name of institution
+        cite (str): citation string
+        abbreviation (str): abbreviation
+        is_US (bool): if institution is on US NSF funding
+        region (Region): a value from the Region enum
+
+    Optional attributes:
+        authorlist (bool): is this an active authorlist inst?
+        authorlists (dict): multiple authorlists with different citations
+        has_mou (bool): is this an active MOU inst?
+
+    Args:
+        experiment (str): experiment name
+        institution (str): institution name
+        attrs (dict): attributes
+    """
+    attrs = validate_attrs(attrs)
+    authorlists = attrs.pop('authorlists', None)
+
+    group_path = f'/institutions/{experiment}/{institution}'
+    await groups.create_group(group_path, attrs=attrs, rest_client=rest_client)
+
+    if authorlists:
+        for name in authorlists:
+            attrs = {'cite': authorlists[name]}
+            await groups.create_group(f'{group_path}/authorlist-{name}',
+                                      attrs=attrs, rest_client=rest_client)
+    elif attrs['authorlist']:
+        await groups.create_group(f'{group_path}/authorlist', rest_client=rest_client)
+
+async def modify_inst(experiment, institution, attrs={}, rest_client=None):
+    """
+    Modify attributes for an institution.
+
+    Patches attributes with existing ones.  Use `None` to remove attr.
+
+    Does not handle authorlist subgroups.
+
+    Args:
+        experiment (str): experiment name
+        institution (str): institution name
+        attrs (dict): attributes to modify
+    """
+    authorlists = attrs.pop('authorlists', None)
+
+    existing_attrs = await inst_info(experiment, institution, rest_client=rest_client)
+    existing_authorlists = existing_attrs.pop('authorlsits', None)
+
+    new_attrs = existing_attrs.copy()
+    new_attrs.update(attrs)
+
+    new_attrs = validate_attrs(new_attrs)
+
+    group_path = f'/institutions/{experiment}/{institution}'
+    await groups.modify_group(group_path, attrs=new_attrs, rest_client=rest_client)
+
+
+def main():
+    import argparse
+    from pprint import pprint
+
+    parser = argparse.ArgumentParser(description='Keycloak institution management')
+    subparsers = parser.add_subparsers()
+    parser_list = subparsers.add_parser('list', help='list institutions by group path')
+    parser_list.add_argument('--experiment', help='experiment name')
+    parser_list.set_defaults(func=list_insts)
+    parser_list_flat = subparsers.add_parser('list-name', help='list institutions by name only')
+    parser_list_flat.add_argument('--experiment', help='experiment name')
+    parser_list_flat.set_defaults(func=list_insts_flat)
+    parser_info = subparsers.add_parser('info', help='institution info')
+    parser_info.add_argument('experiment', help='experiment name')
+    parser_info.add_argument('institution', help='institution name')
+    parser_info.set_defaults(func=inst_info)
+    parser_create = subparsers.add_parser('create', help='create a new institution')
+    parser_create.add_argument('experiment', help='experiment name')
+    parser_create.add_argument('institution', help='institution name')
+    parser_create.add_argument('attrs', type=json.loads, help='attributes')
+    parser_create.set_defaults(func=create_inst)
+    parser_modify = subparsers.add_parser('modify', help='modify an institution\'s attributes')
+    parser_modify.add_argument('experiment', help='experiment name')
+    parser_modify.add_argument('institution', help='institution name')
+    parser_modify.add_argument('attrs', type=json.loads, help='attributes')
+    parser_modify.set_defaults(func=modify_inst)
+    args = vars(parser.parse_args())
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    rest_client = get_rest_client()
+    func = args.pop('func')
+    ret = asyncio.run(func(rest_client=rest_client, **args))
+    if ret is not None:
+        pprint(ret)
+
+if __name__ == '__main__':
+    main()
---
+++ tests/krs/test_institutions.py
@@ -0,0 +1,113 @@
+import pytest
+
+from krs.token import get_token
+from krs import groups, institutions, users
+
+from ..util import keycloak_bootstrap
+
+@pytest.mark.asyncio
+async def test_list_insts(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+
+    # first test with no insts
+    ret = await institutions.list_insts(rest_client=keycloak_bootstrap)
+    assert ret == {}
+
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test', rest_client=keycloak_bootstrap)
+    ret = await institutions.list_insts(rest_client=keycloak_bootstrap)
+    assert ret == {'/institutions/IceCube/Test': {}}
+
+    ret = await institutions.list_insts('IceCube', rest_client=keycloak_bootstrap)
+    assert ret == {'/institutions/IceCube/Test': {}}
+
+    ret = await institutions.list_insts('Other', rest_client=keycloak_bootstrap)
+    assert ret == {}
+
+@pytest.mark.asyncio
+async def test_list_insts_flat(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+
+    # first test with no insts
+    ret = await institutions.list_insts_flat(rest_client=keycloak_bootstrap)
+    assert ret == {}
+
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube2', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube2/Test', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube2/Test2', rest_client=keycloak_bootstrap)
+    ret = await institutions.list_insts_flat(rest_client=keycloak_bootstrap)
+    assert ret == {'Test': {}, 'Test2': {}}
+
+    ret = await institutions.list_insts_flat('IceCube', rest_client=keycloak_bootstrap)
+    assert ret == {'Test': {}}
+
+    ret = await institutions.list_insts_flat('Other', rest_client=keycloak_bootstrap)
+    assert ret == {}
+
+    await groups.create_group('/institutions/IceCube/Test2', {'foo': 'bar'}, rest_client=keycloak_bootstrap)
+    with pytest.raises(institutions.InstitutionAttrsMismatchError):
+        await institutions.list_insts_flat(rest_client=keycloak_bootstrap)
+
+@pytest.mark.asyncio
+async def test_list_insts_filter(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test', {'foo': '1'}, rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test2', {'foo': '2'}, rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test3', {'bar': '2'}, rest_client=keycloak_bootstrap)
+    ret = await institutions.list_insts_flat(filter_func=lambda n,a: a.get('foo') == '1', rest_client=keycloak_bootstrap)
+    assert ret == {'Test': {'foo': '1'}}
+
+@pytest.mark.asyncio
+async def test_inst_info(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube/Test', {'foo': '1'}, rest_client=keycloak_bootstrap)
+    ret = await institutions.inst_info('IceCube', 'Test', rest_client=keycloak_bootstrap)
+    assert ret == {'foo': '1'}
+
+    await groups.create_group('/institutions/IceCube/Test/authorlist-foo', {'cite': 'abc'}, rest_client=keycloak_bootstrap)
+    ret = await institutions.inst_info('IceCube', 'Test', rest_client=keycloak_bootstrap)
+    assert ret == {'foo': '1', 'authorlists': {'foo': 'abc'}}
+
+@pytest.mark.asyncio
+async def test_create_inst(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+
+    attrs = {'name': 'Test', 'cite': 'Test', 'abbreviation': 'T', 'is_US': False, 'region': institutions.Region.NORTH_AMERICA}
+    await institutions.create_inst('IceCube', 'Test', attrs, rest_client=keycloak_bootstrap)
+    ret = await institutions.list_insts_flat(rest_client=keycloak_bootstrap)
+    assert list(ret.keys()) == ['Test']
+
+    attrs = {'name': 'Test2', 'cite': 'Test2', 'abbreviation': 'TT', 'is_US': False, 'region': 'Asia Pacific'}
+    await institutions.create_inst('IceCube', 'Test2', attrs, rest_client=keycloak_bootstrap)
+    ret = await institutions.list_insts_flat(rest_client=keycloak_bootstrap)
+    assert list(ret.keys()) == ['Test', 'Test2']
+
+    with pytest.raises(ValueError):
+        await institutions.create_inst('IceCube', 'Test3', {}, rest_client=keycloak_bootstrap)
+
+    with pytest.raises(KeyError):
+        await institutions.create_inst('IceCube', 'Test3', {'name':'Test3'}, rest_client=keycloak_bootstrap)
+
+    attrs = {'name': 'Test2', 'cite': 'Test2', 'abbreviation': 'TT', 'is_US': False, 'region': 'Bad'}
+    with pytest.raises(ValueError):
+        await institutions.create_inst('IceCube', 'Test3', attrs, rest_client=keycloak_bootstrap)
+
+@pytest.mark.asyncio
+async def test_modify_inst(keycloak_bootstrap):
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/IceCube', rest_client=keycloak_bootstrap)
+
+    attrs = {'name': 'Test', 'cite': 'Test', 'abbreviation': 'T', 'is_US': False, 'region': institutions.Region.NORTH_AMERICA}
+    await institutions.create_inst('IceCube', 'Test', attrs, rest_client=keycloak_bootstrap)
+
+    attrs = {'has_mou': 'true'}
+    await institutions.modify_inst('IceCube', 'Test', attrs, rest_client=keycloak_bootstrap)
+
+    ret = await institutions.inst_info('IceCube', 'Test', rest_client=keycloak_bootstrap)
+    assert 'has_mou' in ret
+    assert ret['has_mou'] == 'true'
