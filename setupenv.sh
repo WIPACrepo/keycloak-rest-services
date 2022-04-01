3de83e67c65679d6bd199e6f182e743ffcc75a61
David Schultz <david.schultz@icecube.wisc.edu>
2022-04-01 17:32:33 -0500

    first try at new wipac ci/cd action

--- setupenv.sh
+++ setupenv.sh
@@ -4,4 +4,4 @@ python3 -m virtualenv -p python3 env
 echo "unset PYTHONPATH" >> env/bin/activate
 . env/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -e .[tests]
