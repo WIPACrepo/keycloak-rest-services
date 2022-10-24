eadb59c92a056cdf59da3c8ee1d05d116f6eb994
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2022-10-24 10:21:06 -0500

    Update readme (#63)
    * update testing section in README.md
    
    * <bot> update requirements.txt
    
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ resources/pytest-env.sh
@@ -0,0 +1,7 @@
+export KEYCLOAK_URL=http://localhost:8080
+export KEYCLOAK_USERNAME=admin
+export KEYCLOAK_PASSWORD=admin
+
+export LDAP_URL=ldap://localhost:1389
+export LDAP_ADMIN_USER=cn=admin,dc=icecube,dc=wisc,dc=edu
+export LDAP_ADMIN_PASSWORD=admin
