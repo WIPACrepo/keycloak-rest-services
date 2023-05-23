8441b03e241a577361bdfb591a4edeea81e67dce
Vladimir Brik <vbrik@icecube.wisc.edu>
2023-05-23 13:20:01 -0500

    add action to remove departed users from mailing lists

---
+++ tests/actions/test_deprovision_mailing_lists.py
@@ -0,0 +1,29 @@
+import pytest
+
+from ..util import keycloak_bootstrap
+from krs import groups, institutions, users
+
+from actions.deprovision_mailing_lists import process
+
+
+@pytest.mark.asyncio
+async def test_deprovision_mailing_lists(keycloak_bootstrap):
+    await users.create_user('u1', first_name='first', last_name='last', email='email1@test',
+                            rest_client=keycloak_bootstrap)
+    await users.create_user('u2', first_name='first', last_name='last', email='email2@test',
+                            rest_client=keycloak_bootstrap)
+
+    await groups.create_group('/mailing-lists', rest_client=keycloak_bootstrap)
+    await groups.create_group('/mailing-lists/ml', rest_client=keycloak_bootstrap)
+    await groups.add_user_group('/mailing-lists/ml', 'u1', rest_client=keycloak_bootstrap)
+    await groups.add_user_group('/mailing-lists/ml', 'u2', rest_client=keycloak_bootstrap)
+
+    await groups.create_group('/institutions', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/i', rest_client=keycloak_bootstrap)
+    await groups.create_group('/institutions/i/l', rest_client=keycloak_bootstrap)
+    await groups.add_user_group('/institutions/i/l', 'u1', rest_client=keycloak_bootstrap)
+
+    await process('/mailing-lists', keycloak_bootstrap)
+    ret = await groups.get_group_membership('/mailing-lists/ml', rest_client=keycloak_bootstrap)
+    assert ret == ['u1']
+
