b2c35fb8c8a93ecc76ad49293af3f38330ec1c4a
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2023-08-02 16:19:34 -0500

    handling mailing_list_email attr in sync_gws_mailing_lists.py (#94)
    * implement handling of mailing_list_email attr in sync_gws_mailing_lists.py
    
    * <bot> update requirements-actions.txt
    
    * <bot> update requirements-tests.txt
    
    * <bot> update requirements.txt
    
    * update requirements-actions.txt
    
    * <bot> update requirements-actions.txt
    
    * add asyncache cachetools to setup.cfg
    
    * <bot> update setup.cfg
    
    * <bot> update requirements-actions.txt
    
    * update setup.cfg
    
    * <bot> update requirements-actions.txt
    
    * update test_sync_gws_mailing_lists.py
    
    * use python 3.9
    
    * improve log msg format
    
    * add option to specify which list to sync
    
    * Revert "add option to specify which list to sync"
    
    This reverts commit 330a3d680b052250934d423c13a2e4eed6bed7bc.
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>

--- tests/actions/test_sync_gws_mailing_lists.py
+++ tests/actions/test_sync_gws_mailing_lists.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock, call
 
 from ..util import keycloak_bootstrap  # noqa: F401
 from krs.groups import create_group, modify_group, add_user_group
-from krs.users import create_user
+from krs.users import create_user, modify_user
 
 from actions.sync_gws_mailing_lists import sync_gws_mailing_lists
 
@@ -15,7 +15,7 @@ async def test_sync_gws_mailing_lists_insert(keycloak_bootstrap):  # noqa: F811
     request_members.execute = MagicMock(
         return_value={'members': [
             {'email': 'keep.keep@icecube.wisc.edu', 'role': 'MEMBER'},
-            {'email': 'owner@test', 'role': 'OWNER'}]})
+            {'email': 'owner-dont-delete@test', 'role': 'OWNER'}]})
     gws_members_client = MagicMock()
     gws_members_client.list = MagicMock(return_value=request_members)
     gws_members_client.list_next = MagicMock(return_value=None)
@@ -36,10 +36,21 @@ async def test_sync_gws_mailing_lists_insert(keycloak_bootstrap):  # noqa: F811
     await create_user('add', first_name='add', last_name='add', email='add@test', rest_client=keycloak_bootstrap)
     await add_user_group('/mail/list', 'add', rest_client=keycloak_bootstrap)
 
+    await create_user('custom', first_name='custom', last_name='custom', email='custom@test', rest_client=keycloak_bootstrap)
+    await modify_user('custom', attribs={'mailing_list_email':'custom@ext'}, rest_client=keycloak_bootstrap)
+    await add_user_group('/mail/list', 'custom', rest_client=keycloak_bootstrap)
+
     await sync_gws_mailing_lists(gws_members_client, gws_groups_client, keycloak_bootstrap, dryrun=False)
 
     assert gws_members_client.insert.call_args_list == \
            [call(groupKey='test@gws', body={'email': 'add.add@icecube.wisc.edu',
+                                            'delivery_settings': 'ALL_MAIL',
+                                            'role': 'MEMBER'}),
+            call(groupKey='test@gws', body={'email': 'custom@ext',
+                                            'delivery_settings': 'ALL_MAIL',
+                                            'role': 'MEMBER'}),
+            call(groupKey='test@gws', body={'email': 'custom.custom@icecube.wisc.edu',
+                                            'delivery_settings': 'NONE',
                                             'role': 'MEMBER'})]
     assert gws_members_client.delete.call_count == 0
     assert gws_members_client.patch.call_count == 0
@@ -51,7 +62,7 @@ async def test_sync_gws_mailing_lists_delete(keycloak_bootstrap):  # noqa: F811
     request_members.execute = MagicMock(
         return_value={'members': [
             {'email': 'keep.keep@icecube.wisc.edu', 'role': 'MEMBER'},
-            {'email': 'owner@test', 'role': 'OWNER'},
+            {'email': 'owner-dont-delete@test', 'role': 'OWNER'},
             {'email': 'remove@test', 'role': 'MEMBER'}]})
     gws_members_client = MagicMock()
     gws_members_client.list = MagicMock(return_value=request_members)
