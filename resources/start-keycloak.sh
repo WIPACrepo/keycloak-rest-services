0655b1b1cc7d75be3c4884b6f36841e98cc0152d
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2023-11-08 11:54:13 -0600

    make local test environment match what we have in github

--- resources/start-keycloak.sh
+++ resources/start-keycloak.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
+docker build -t krs-test-keycloak -f ./resources/keycloak-image/Dockerfile .
+
 docker run -d --name keycloak --network host \
- -v $PWD/resources/keycloak-to-rabbit-1.0.jar:/opt/jboss/keycloak/standalone/deployments/keycloak-to-rabbit-1.0.jar \
- -v $PWD/keycloak_theme:/opt/jboss/keycloak/themes/custom \
- --env KK_TO_RMQ_USERNAME=admin --env KK_TO_RMQ_PASSWORD=admin \
- --env KK_TO_RMQ_VHOST=keycloak \
- jboss/keycloak:15.0.1 -Djboss.bind.address.private=127.0.0.1 -Djboss.bind.address=127.0.0.1
-sleep 60
-docker exec keycloak /opt/jboss/keycloak/bin/add-user-keycloak.sh -u admin -p admin
-docker restart keycloak
+  --env KEYCLOAK_ADMIN=admin --env KEYCLOAK_ADMIN_PASSWORD=admin \
+  krs-test-keycloak start-dev
 
 until curl http://localhost:8080 >/dev/null 2>/dev/null
 do
-    sleep 5
+    sleep 1
 done
 
 echo "Keycloak Ready"
