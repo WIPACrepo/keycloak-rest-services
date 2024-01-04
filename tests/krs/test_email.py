d9213e562bac94f78fa6765198c1570fd29da86f
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2024-01-04 11:54:54 -0600

    add test_send_email_no_domain

--- tests/krs/test_email.py
+++ tests/krs/test_email.py
@@ -12,9 +12,11 @@ class MockResponse(AbstractContextManager, MagicMock):
         pass
     send_message = MagicMock()
 
+
 @pytest.fixture
 def patch_smtp(mocker):
     return mocker.patch('smtplib.SMTP', new=MockResponse())
+
 
 def test_send_email(patch_smtp):
     krs.email.send_email('foo@bar', 'test', 'test message')
@@ -23,5 +25,16 @@ def test_send_email(patch_smtp):
     patch_smtp.return_value.send_message.assert_called()
     a = patch_smtp.return_value.send_message.call_args[0][0]
     assert 'To: foo@bar' in str(a)
+    assert 'Subject: test' in str(a)
+    assert 'test message' in str(a)
+
+
+def test_send_email_no_domain(patch_smtp):
+    krs.email.send_email('foo', 'test', 'test message')
+
+    patch_smtp.assert_called()
+    patch_smtp.return_value.send_message.assert_called()
+    a = patch_smtp.return_value.send_message.call_args[0][0]
+    assert 'To: foo' in str(a)
     assert 'Subject: test' in str(a)
     assert 'test message' in str(a)
