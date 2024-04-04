10049de6fc9a9b09116daeb9bb40df995723eb04
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2024-04-04 15:07:43 -0500

    fix

--- krs/util.py
+++ krs/util.py
@@ -14,8 +14,10 @@ async def keycloak_version(rest_client):
     """
     saved_addr = rest_client.address
     addr_parts = saved_addr.split('/')
+    # make the new address look like https://fqdn/auth/admin
+    new_address = '/'.join(addr_parts[:3]) + '/auth/admin'
     try:
-        rest_client.address = '/'.join(addr_parts[:3]) + '/auth/admin'
+        rest_client.address = new_address
         server_info = await rest_client.request('GET', '/serverinfo')
     finally:
         rest_client.address = saved_addr
