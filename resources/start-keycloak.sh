8154efecc0ceef5be5795c10a1b643c2e60a8916
David Schultz <david.schultz@icecube.wisc.edu>
2025-05-09 17:04:36 -0500

    add readme for custom jars

--- resources/start-keycloak.sh
+++ resources/start-keycloak.sh
@@ -7,8 +7,8 @@ podman run --rm -it --name keycloak --network host \
   --env KC_BOOTSTRAP_ADMIN_USERNAME=admin --env KC_BOOTSTRAP_ADMIN_PASSWORD=admin \
   --env WLCG_AUTH_URL=http://localhost:8888/auth \
   --env WLCG_AUTH_SECRET=secret \
-  -v $PWD/resources/test-master-realm.json:/opt/keycloak/data/import/master-realm.json \
-  krs-test-keycloak start-dev  --import-realm
+  #-v $PWD/resources/test-master-realm.json:/opt/keycloak/data/import/master-realm.json \
+  krs-test-keycloak start-dev  #--import-realm
 
 until curl http://localhost:8080 >/dev/null 2>/dev/null
 do
