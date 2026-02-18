f7d0c6320ead56bcabe542400a7171a1b90082b9
David Schultz <david.schultz@icecube.wisc.edu>
2026-02-18 10:55:39 -0600

    add users to csv script

---
+++ scripts/get_users_csv.py
@@ -0,0 +1,70 @@
+"""
+Look up all users and write them to a csv file.
+
+Examples::
+
+    ./setupenv.sh
+    . env/bin/activate
+    python scripts/get_users_csv.py users.csv
+"""
+
+import asyncio
+import csv
+import logging
+import sys
+
+from asyncache import cached
+from cachetools import TTLCache
+
+from krs.token import get_rest_client
+from krs.users import list_users, user_info
+from krs.groups import list_groups
+
+
+@cached(TTLCache(1024, 60))
+async def get_name(username, client=None):
+    ret = await user_info(username, rest_client=client)
+    return ret['firstName']+' '+ret['lastName']
+
+
+async def run(client, filename):
+    krs_users = await list_users(rest_client=client)
+    #krs_groups = await list_groups(rest_client=client)
+    ret = {}
+    data = {}
+    fieldnames = set()
+    for user in krs_users:
+        data = krs_users[user]
+        data.update(data.pop('attributes',{}))
+        ret[user] = data
+        fieldnames.update(data.keys())
+
+    if ret:
+        #fieldnames = list(fieldnames)
+        with open(filename, 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(ret.values())
+            #for data in ret.values():
+                #row = [data[k] if k in data else '' for k in fieldnames]
+                #writer.writerow(row)
+    else:
+        logging.warning('no users found!')
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='csv filename to write to')
+    parser.add_argument('--log-level', default='warning', choices=('debug', 'info', 'warning', 'error'), help='logging level')
+    args = vars(parser.parse_args())
+
+    logging.basicConfig(level=getattr(logging, args['log_level'].upper()))
+
+    keycloak_client = get_rest_client()
+
+    asyncio.run(run(client=keycloak_client, filename=args['filename']))
+
+
+if __name__ == '__main__':
+    main()
