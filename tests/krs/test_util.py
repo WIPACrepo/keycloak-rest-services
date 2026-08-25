044959a189ada17afa13696946e4802ab2b48055
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2026-08-25 13:37:54 -0500

    Support newer keycloak 26.7.2
    * Support newer keycloak
    
    * try 26.4.15
    
    * resources/keycloak-image/Dockerfile updated
    
    * resources/keycloak-image/Dockerfile updated
    
    * keycloak_version() fixup... do we need this function?
    
    * tests/krs/test_util.py updated
    
    * resources/keycloak-image/Dockerfile updated
    
    * resources/keycloak-image/Dockerfile updated

--- tests/krs/test_util.py
+++ tests/krs/test_util.py
@@ -1,16 +1,26 @@
+import os
+
 import pytest
-
 from packaging import version
+from rest_tools.client import RestClient
 
-from krs import util
+from krs import bootstrap, util
 
-from ..util import keycloak_bootstrap  # type: ignore
+from ..util import keycloak_bootstrap  # type: ignore # noqa: F401
 
 
 @pytest.mark.asyncio
-async def test_keycloak_version(keycloak_bootstrap):
-    ver = await util.keycloak_version(rest_client=keycloak_bootstrap)
+async def test_keycloak_version(keycloak_bootstrap):  # noqa: F811
+    # As of Keycloak 26.7, /serverinfo only includes systemInfo (which carries
+    # the version) for accounts holding manage-realm in the master realm, so
+    # a realm-scoped service account like keycloak_bootstrap's can't see it.
+    master_admin = RestClient(
+        f'{os.environ["KEYCLOAK_URL"]}/auth/admin/realms/master',
+        token=bootstrap.get_token(),
+        retries=0,
+    )
+    ver = await util.keycloak_version(rest_client=master_admin)
     version.parse(ver)
     # Check that our rest client is still functional
-    ret = await keycloak_bootstrap.request('GET', '/')
+    ret = await master_admin.request('GET', '/')
     assert 'realm' in ret
